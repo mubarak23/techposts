83dafe3cfee9c9c6afb02ac698c1e0917c51f676
mubarak23 <mubarakaminu340@gmail.com>
2020-05-14 21:34:52 +0100

    signin logic

--- src/components/Auth/Signin.js
+++ src/components/Auth/Signin.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import M from 'materialize-css';
 import './../App.css';
 
 const Signin = () => {
@@ -7,13 +8,53 @@ const Signin = () => {
   const [username, SetUsername] = useState();
   const [password, SetPassword] = useState();
 
+  const PostSignin = () => {
+    fetch('https://fast-plains-95666.herokuapp.com/api/user', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'Application/json',
+      },
+      body: {
+        username,
+        password,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.errors) {
+          M.toast({
+            html: 'An Error Has Occur Try Again',
+            classes: '#c62828 red darken-3',
+          });
+        } else {
+          M.toast({ html: data.message, classes: '#43a047 green darken-1' });
+          history.push('/signin');
+        }
+      })
+      .catch((errors) => {
+        console.log(errors);
+      });
+  };
+
   return (
     <div className='my-card'>
       <div className='card auth-card'>
         <h3>Techpost Signin</h3>
-        <input type='text' placeholder='Username' />
-        <input type='password' placeholder='Enter Your Password' />
+        <input
+          type='text'
+          value={username}
+          onChange={(e) => SetUsername(e.target.value)}
+          placeholder='Username'
+        />
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => SetPassword(e.target.value)}
+          placeholder='Enter Your Password'
+        />
         <button
+          onClick={() => PostSignin()}
           className='btn waves-effect
          waves-light #64b5f6 blue darken-1'
         >
