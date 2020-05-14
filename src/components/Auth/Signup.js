b97b34f7e46a6ab168f0d3c073fbed98eacb3a86
mubarak23 <mubarakaminu340@gmail.com>
2020-05-14 17:36:47 +0100

    post signup data

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -8,16 +8,55 @@ const Signup = () => {
   const [email, Setemail] = useState('');
   const [password, SetPassword] = useState('');
 
+  //make post request
+  const postSignup = () => {
+    fetch('/signup', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        history.push('/signin');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className='my-card'>
       <div className='card auth-card'>
         <h3>Techpost Signup</h3>
-        <input type='text' placeholder='Username' />
-        <input type='text' placeholder='Email Address' />
-        <input type='password' placeholder='Enter a Unique Password' />
+        <input
+          type='text'
+          placeholder='Username'
+          value={username}
+          onChange={(e) => SetUsername(e.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='Email Address'
+          value={email}
+          onChange={(e) => Setemail(e.target.value)}
+        />
+        <input
+          type='password'
+          placeholder='Enter a Unique Password'
+          value={password}
+          onChange={(e) => SetPassword(e.target.value)}
+        />
         <button
           className='btn waves-effect 
           waves-light #64b5f6 blue darken-1'
+          onClick={() =>postSignup()}
         >
           Signup
         </button>
