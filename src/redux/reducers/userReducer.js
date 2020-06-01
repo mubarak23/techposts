3e738bdc0dcbce79f38a3c5379347e533cd91b5d
mubarak23 <mubarakaminu340@gmail.com>
2020-06-01 13:27:49 +0100

    did not work

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,7 +1,17 @@
 export const intialState = null;
 export const reducer = (state, action) => {
-  if (action.type == 'User') {
+  if (action.type == 'USER') {
     return action.payload;
+  }
+  if (action.type == 'CLEAR') {
+    return null;
+  }
+  if (action.type == 'UPDATE') {
+    return {
+      ...state,
+      followers: action.payload.followers,
+      following: action.payload.following,
+    };
   }
   return state;
 };
